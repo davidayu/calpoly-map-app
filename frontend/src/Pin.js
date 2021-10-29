f3005ca808c718f0752524cf7d946ca65997a84c
ksittner <kevinsittner6@gmail.com>
2021-10-29 12:46:18 -0700

    Working map component and new pin form completed

---
+++ frontend/src/Pin.js
@@ -0,0 +1,19 @@
+
+import { Marker, useMapEvents } from "react-leaflet";
+import {Icon} from 'leaflet'
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+
+function Pin(props) {
+ 
+   useMapEvents({
+     click: (event) => {
+        props.handleMapClick(event.latlng);
+     },
+   });
+
+   return (props.position === null ? null : 
+      <Marker position={props.position} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})}></Marker>
+    );
+}
+
+export default Pin;
