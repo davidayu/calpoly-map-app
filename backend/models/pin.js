c64f8b132af71b965b5f1963ce962eb95a9efd9f
Ameya Malekar <ameya_malekar@Ameyas-MacBook-Pro.local>
2021-11-01 14:58:56 -0700

    Updated pin schema

--- backend/models/pin.js
+++ backend/models/pin.js
@@ -11,11 +11,43 @@ const pinSchema = new mongoose.Schema({
     required: true,
     trim: true,
   },
-  z: {
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  upvotes: {
     type: Number,
     required: true,
     trim: true,
   },
+  downvotes: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  pinType: {
+    type: String,
+    enum: ['STUDY', 'ART', 'DINING', 'NONE'],
+    default: 'NONE',
+    trim: true,
+  },
+  indoor: {
+    type: Boolean,
+    required: true,
+    trim: true,
+  },
+  pinState: {
+    type: String,
+    enum: ['NEW', 'MINIMIZED', 'MAXIMIZED'],
+    default: 'NEW',
+    trim: true,
+  }, 
 });
 
 const Pin = mongoose.model("Pin", pinSchema);
