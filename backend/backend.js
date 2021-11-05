6e2e5a31af9edbccdb744fa09df137b249efec17
Noah Marx <nmarx87@gmail.com>
2021-11-05 16:46:24 -0700

    prettified code

--- backend/backend.js
+++ backend/backend.js
@@ -3,71 +3,71 @@ const cors = require("cors");
 const pinServices = require("./models/pin-services");
 const app = express();
 const port = 5000;
- 
+
 app.use(express.json());
- 
+
 app.get("/", (req, res) => {
- res.send("Hello World!");
+  res.send("Hello World!");
 });
- 
+
 app.get("/pins", async (req, res) => {
- const lat = req.query.lat;
- const lon = req.query.lon;
- let result = await pinServices.getPins(lat, lon);
- result = { pins_list: result };
- res.send(result);
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  let result = await pinServices.getPins(lat, lon);
+  result = { pins_list: result };
+  res.send(result);
 });
- 
+
 app.post("/pins", async (req, res) => {
- const pin = req.body;
- const savedPin = await pinServices.addPin(pin);
- if (savedPin) {
-   res.status(201).send(savedPin);
- } else {
-   res.status(500).end();
- }
+  const pin = req.body;
+  const savedPin = await pinServices.addPin(pin);
+  if (savedPin) {
+    res.status(201).send(savedPin);
+  } else {
+    res.status(500).end();
+  }
 });
- 
+
 app.delete("/pins/:id", async (req, res) => {
- const id = req.params["id"];
- let pin = await pinServices.findPinById(id);
- if (pin === undefined) res.status(404).send("Pin not found.");
- else {
-   await pinServices.removePin(id);
-   res.status(204).end();
- }
+  const id = req.params["id"];
+  let pin = await pinServices.findPinById(id);
+  if (pin === undefined) res.status(404).send("Pin not found.");
+  else {
+    await pinServices.removePin(id);
+    res.status(204).end();
+  }
 });
- 
+
 app.put("/pins/upvote/:id", async (req, res) => {
- const id = req.params["id"];
- let pin = await pinServices.findPinById(id);
- if (pin === undefined) res.status(404).send("Pin type not found.");
- else {
-   await pinServices.upvotePin(id);
-   res.status(204).end();
- }
+  const id = req.params["id"];
+  let pin = await pinServices.findPinById(id);
+  if (pin === undefined) res.status(404).send("Pin type not found.");
+  else {
+    await pinServices.upvotePin(id);
+    res.status(204).end();
+  }
 });
- 
+
 app.put("/pins/downvote/:id", async (req, res) => {
- const id = req.params["id"];
- let pin = await pinServices.findPinById(id);
- if (pin === undefined) res.status(404).send("Pin not found.");
- else {
-   await pinServices.downvotePin(id);
-   res.status(204).end();
- }
+  const id = req.params["id"];
+  let pin = await pinServices.findPinById(id);
+  if (pin === undefined) res.status(404).send("Pin not found.");
+  else {
+    await pinServices.downvotePin(id);
+    res.status(204).end();
+  }
 });
- 
+
 app.get("/pins/:type", async (req, res) => {
- const pinType = req.params['type'];
- let result = await pinServices.filterByType(pinType);
- if (result === undefined || result === null)
-       res.status(404).send('Resource not found.');
-   else {
-       res.send(result);
-   }
+  const pinType = req.params["type"];
+  let result = await pinServices.filterByType(pinType);
+  if (result === undefined || result === null)
+    res.status(404).send("Resource not found.");
+  else {
+    res.send(result);
+  }
 });
- 
+
 app.listen(port, () => {
- console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
--- backend/models/pin-services.js
+++ backend/models/pin-services.js
@@ -1,89 +1,87 @@
-const dotenv = require('dotenv').config({ path: 'database.env' })
+const dotenv = require("dotenv").config({ path: "database.env" });
 const mongoose = require("mongoose");
 const pinModel = require("./pin");
- 
+
 const uri = process.env.DB_URI;
 mongoose
- .connect(uri, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
- }
- )
- .catch((error) => console.log(error));
- 
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => console.log(error));
+
 async function addPin(pin) {
- try {
-   const pinToAdd = new pinModel(pin);
-   const savedPin = await pinToAdd.save();
-   return savedPin;
- } catch (error) {
-   console.log(error);
-   return false;
- }
+  try {
+    const pinToAdd = new pinModel(pin);
+    const savedPin = await pinToAdd.save();
+    return savedPin;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
- 
+
 async function getPins(lat, lon) {
- let result;
- if (lat === undefined || lon === undefined) {
-   result = await pinModel.find();
- } else {
-   result = await findPinByCoords(lat, lon);
- }
- return result;
+  let result;
+  if (lat === undefined || lon === undefined) {
+    result = await pinModel.find();
+  } else {
+    result = await findPinByCoords(lat, lon);
+  }
+  return result;
 }
- 
+
 async function findPinByCoords(lat, lon) {
- return await pinModel.find({ lat: lat, lon: lon });
+  return await pinModel.find({ lat: lat, lon: lon });
 }
- 
- 
+
 async function findPinById(id) {
- try {
-   return await pinModel.findById(id);
- } catch (error) {
-   console.log(error);
-   return undefined;
- }
+  try {
+    return await pinModel.findById(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
- 
+
 async function removePin(id) {
- return await pinModel.findByIdAndDelete({ _id: id });
+  return await pinModel.findByIdAndDelete({ _id: id });
 }
- 
+
 async function upvotePin(id) {
- let result;
- try {
-   result = await pinModel.findById(id);
-   result.upvotes += 1;
-   await result.save();
- } catch (error) {
-   console.log(error);
-   return undefined;
- }
+  let result;
+  try {
+    result = await pinModel.findById(id);
+    result.upvotes += 1;
+    await result.save();
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
- 
+
 async function downvotePin(id) {
- let result;
- try {
-   result = await pinModel.findById(id);
-   result.downvotes += 1;
-   await result.save();
- } catch (error) {
-   console.log(error);
-   return undefined;
- }
+  let result;
+  try {
+    result = await pinModel.findById(id);
+    result.downvotes += 1;
+    await result.save();
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
- 
+
 async function filterByType(pinType) {
- let result;
- if (pinType === undefined) {
-   result = await pinModel.find();
- } else {
-   result =  await pinModel.find(({ pinType: pinType }));
- }
- return result;
+  let result;
+  if (pinType === undefined) {
+    result = await pinModel.find();
+  } else {
+    result = await pinModel.find({ pinType: pinType });
+  }
+  return result;
 }
- 
+
 exports.addPin = addPin;
 exports.getPins = getPins;
 exports.findPinByCoords = findPinByCoords;
