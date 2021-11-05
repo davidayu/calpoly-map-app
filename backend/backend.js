676d5aced24d4531c37249838ada2bb9aa508a44
Noah Marx <nmarx87@gmail.com>
2021-11-04 22:11:59 -0700

    set up heroku app

--- backend/backend.js
+++ backend/backend.js
@@ -58,6 +58,6 @@ app.put("/pins/downvote/:id", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log("REST API is listening.");
 });
