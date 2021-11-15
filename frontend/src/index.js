c559d645684ddc58f7302d803f9c9758e532c6a9
elias <eliasowilson@gmail.com>
2021-11-15 14:47:43 -0800

    index fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./Home";
-import CommentView from './CommentView';
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
-ReactDOM.render(<CommentView />, document.getElementById('root'));
-
-
-//ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById("root"));
