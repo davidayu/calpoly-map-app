cb0472469db4dcb76e102a9ff9517d2e60f4b4c9
elias <eliasowilson@gmail.com>
2021-11-15 14:20:06 -0800

    comment view frontend done

--- frontend/src/CommentView.js
+++ frontend/src/CommentView.js
@@ -1,4 +1,62 @@
-import { svg } from "leaflet";
+// import { svg } from "leaflet";
+import React, { useState } from "react";
+import Collapsible from 'react-collapsible';
+
+import { ReactComponent as ThumbsUp } from "./thumbs-up.svg";
+import { ReactComponent as ThumbsDown } from "./thumbs-down.svg";
+
+function CommentViewHeader(props){
+    return (
+        <div>
+            <div style={{fontSize: 20}}>{props.pin.title}</div>
+            <div>{props.pin.description}</div>
+            <br></br>
+            <div>Comments:</div>
+        </div>
+    );
+}
+
+function CommentViewBody(props) {
+    const _comments = props.commentsData.map( (text, index) => {
+        return(
+            <Collapsible key={index} trigger = "▼/▲" open = 'true'>
+                {text.description} <br></br>
+                <ThumbsUp title="Upvotes" onClick={() => props.upVote(index)}/> {text.upvotes} 
+                <ThumbsDown title="Downvotes" onClick={() => props.downVote(index)}/> {text.downvotes} 
+            </Collapsible>
+        );
+    });
+    return <div>{_comments}</div>;
+}
+
+function CommentView(){
+    let pin = {title:'Study Bench', description:'Nice stone bench with shade from trees', upVotes:3, downVotes:4,pinType:'Study Location', color:'green'}
+    let commentz = [{description:"I go here all the time", downvotes: 10, upvotes: 50},{description:"I got my shoes dirty here, I don't like it", downvotes: 6, upvotes: 32}]
+
+    const [comments, setComments] = useState(commentz);
+
+    function upVote(index){
+        let commentsTemp = comments;
+        commentsTemp[index].upvotes += 1;
+        setComments([...commentsTemp]);
+    }
+
+    function downVote(index){
+        let commentsTemp = comments;
+        commentsTemp[index].downvotes += 1;
+        setComments([...commentsTemp]);
+    }
+
+    return (
+        <div>
+            <CommentViewHeader pin = {pin}/>
+            <CommentViewBody  commentsData = {comments} upVote = {upVote} downVote = {downVote}/>
+        </div>
+    );
+}
+
+export default CommentView;
+/*import { svg } from "leaflet";
 import React, { useState } from "react";
 import Collapsible from 'react-collapsible';
 
@@ -18,7 +76,7 @@ function CommentView(props) {
         commentz[index].upvotes = commentz[index].upvotes + 1;
         console.log(commentz[index].upvotes);
         addVote(commentz);
-    }
+    }*/
     /*const [show, setShow] = useState(false);
     const handleOpen = () => {
       setShow(!show); // Toggle accordion
@@ -42,7 +100,7 @@ function CommentView(props) {
         console.log(text);
         text.downvotes = text.downvotes + 1;
     }*/
-
+/*
     return(
         <div>
             <div style={{fontSize: 20}} >{pin.title}</div>
@@ -61,7 +119,7 @@ function CommentView(props) {
         </div>
     )
 }
-export default CommentView;
+export default CommentView;*/
 
 //<button className="sign" onClick={handleOpen} style={{float: 'left', margin: 10}}>{show ? '-' : '+'}</button>
 //<div style={{float: 'left', margin: 10}}>{show && text.comment}</div> text.upvotes text.downvotes}
