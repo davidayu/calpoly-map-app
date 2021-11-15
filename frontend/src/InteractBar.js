5232ccf4db8da6e4f5bee66f38cce5ebfb83cdb4
David <dyu22@calpoly.edu>
2021-11-15 15:49:46 -0800

    Added onclick for show list and broken onclick for show location

--- frontend/src/InteractBar.js
+++ frontend/src/InteractBar.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { ReactComponent as ListView } from "./list.svg";
-import { ReactComponent as AddPin } from "./file-plus.svg";
-import { ReactComponent as RemovePin } from "./file-minus.svg";
+import ReactDOM from "react-dom";
+import { ReactComponent as ListIcon } from "./list.svg";
+import { ReactComponent as AddPinIcon } from "./file-plus.svg";
+import { ReactComponent as RemovePinIcon } from "./file-minus.svg";
 import axios from "axios";
+import ListView from "./ListView";
+import LocationForm from "./LocationForm";
+
+function ShowListView() {
+  ReactDOM.render(<ListView />, document.getElementById("root"));
+}
+
+function ShowLocationForm() {
+  //ReactDOM.render(<LocationForm />, document.getElementById("root"));
+}
+
 
 function InteractBar() {
   return (
     <div>
-      <ListView />
-      <AddPin />
-      <RemovePin />
+      <ListIcon onClick={ShowListView} />
+      <AddPinIcon onClick={ShowLocationForm} />
+      <RemovePinIcon />
     </div>
   );
 }
