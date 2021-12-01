61d26d727625174056895376e9f1b4ce1add707f
elias <eliasowilson@gmail.com>
2021-12-01 15:10:03 -0800

    listview connected to backend

--- frontend/src/ListView.js
+++ frontend/src/ListView.js
@@ -1,15 +1,58 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./Home";
 import ReactDOM from "react-dom";
+import axios from "axios";
 
-function goHome(){//add browser router??
+
+import { ReactComponent as ThumbsUp } from "./thumbs-up.svg";
+import { ReactComponent as ThumbsDown } from "./thumbs-down.svg";
+
+function goHome(){
   ReactDOM.render(<Home />, document.getElementById("root"));
 }
 
+
+
 function ListView(props) {
+  
+  async function getAllPins(){
+    try {
+       const response = await axios.get('http://localhost:5000/pins');
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+  }
+  
+  async function upvotePin(id){
+    try {
+       const response = await axios.put('http://localhost:5000/pins/upvote/' + id, id);
+       setList(response.data);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+  }
+  
+  async function downvotePin(id){
+    try {
+       const response = await axios.put('http://localhost:5000/pins/downvote/' + id, id);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+  }
+
   //let listitems = getPins();
 
-  let listitems = [
+  const [listItems, setList] = useState([]);
+  /*let listItems = [
     {
       title: "Study Bench",
       description: "Nice stone bench with shade from trees",
@@ -26,19 +69,32 @@ function ListView(props) {
       pinType: "Eating Location",
       color: "red",
     },
-  ];
+  ];*/
+  
+
+    useEffect(() => {
+      getAllPins().then( result => {
+        if (result)
+            setList(result.data.pins_list);
+            console.log(result.data);
+      });
+  }, [] );
+
+  //listItems = getAllPins();
+  console.log('access');
+  //console.log(getAllPins());
 
   return (
     <div>
       <h1>List view</h1>
       <button onClick={goHome} style={{position: 'absolute',right: 0,top: 0,}}> X  </button>
       <p>
-        {listitems.map((pin) => (
+        {listItems.map((pin) => (
           <p style={{ outlineStyle: "groove", outlineColor: pin.color }}>
             <p style={{ fontSize: 20 }}> {pin.title}</p>
             <div>{pin.description}</div>
-            <div style={{ float: "right", margin: 10 }}>
-              Upvotes: {pin.upVotes} Downvotes: {pin.downVotes}{" "}
+            <div style={{ float: "right", margin: 1 }}>
+              <ThumbsUp onClick={() => upvotePin(pin._id)}/> {pin.upvotes} <ThumbsDown onClick={() => downvotePin(pin._id)}/> {pin.downvotes}{" "}
             </div>
             <p style={{ color: pin.color }}>Type: {pin.pinType}</p>
           </p>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./Home";
+import CommentView from "./CommentView"
+import ListView from "./ListView"
 import "./index.css";
 import "leaflet/dist/leaflet.css";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(<ListView />, document.getElementById("root"));
