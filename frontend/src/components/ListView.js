17b1e507d5ab57c22edcd888ac161d687a2e1478
Kevin Sittner <kevinsittner6@gmail.com>
2021-12-04 21:27:32 -0800

    Styled ListView component

--- frontend/src/components/ListView.js
+++ frontend/src/components/ListView.js
@@ -3,7 +3,14 @@ import axios from "axios";
 import { ReactComponent as Close } from "../icons/close.svg";
 import { ReactComponent as ThumbsUp } from "../icons/thumbs-up.svg";
 import { ReactComponent as ThumbsDown } from "../icons/thumbs-down.svg";
-import { useNavigate } from "react-router-dom";
+import { ReactComponent as StudyIcon } from "../icons/study.svg";
+import { ReactComponent as ArtIcon } from "../icons/brush.svg";
+import { ReactComponent as DiningIcon } from "../icons/eating.svg";
+import { ReactComponent as GenericIcon } from "../icons/marker.svg";
+import { ReactComponent as MaximizeIcon } from "../icons/maximize.svg";
+import { pinTypesMap } from "../pinTypes.js";
+import { Link, useNavigate } from "react-router-dom";
+import style from "../styles/ListView.module.css";
 
 function ListView() {
 
@@ -30,6 +37,33 @@ function ListView() {
       }
    }
 
+   function TypeIcon(props) {
+      switch (props.type) {
+        case "STUDY":
+          return (
+            <StudyIcon
+              className={style.cardSubicon}
+              title={pinTypesMap[props.type]["text"]}
+            />
+          );
+        case "ART":
+          return (
+            <ArtIcon
+              className={style.cardSubicon}
+              title={pinTypesMap[props.type]["text"]}
+            />
+          );
+        case "DINING":
+          return (
+            <DiningIcon
+              className={style.cardSubicon}
+              title={pinTypesMap[props.type]["text"]}
+            />
+          );
+        default:
+          return <GenericIcon className={style.cardSubicon} />;
+      }
+    }
 
    function upvoteLocation(index) {
       const id = locations[index]["_id"];
@@ -93,23 +127,51 @@ function ListView() {
     }
 
   return (
-    <div>
-      <h1>List view</h1>
-      <button onClick={() => navigate(-1)} >
-         <Close />
-      </button>
-      <p>
-        {locations.map((pin, index) => (
-          <p style={{ outlineStyle: "groove", outlineColor: pin.color }}>
-            <p style={{ fontSize: 20 }}> {pin.title}</p>
-            <div>{pin.description}</div>
-            <div style={{ float: "right", margin: 1 }}>
-              <ThumbsUp onClick={() => upvoteLocation(index)}/> {pin.upvotes} <ThumbsDown onClick={() => downvoteLocation(index)}/> {pin.downvotes}{" "}
+    <div className={style.listView}>
+      <div className={style.header}>
+         <h2>Locations</h2>
+         <button className={style.iconWrapper}
+            onClick={() => navigate(-1)} 
+         >
+            <Close className={style.icon}/>
+         </button>
+      </div>
+      <div class={style.locationsWrapper}>
+         <div className={style.locations}>
+         {locations.map((pin, index) => (
+            <div className={style.locationCard}>
+               <div className={style.cardTopRow}>
+                  <h4> {pin.title}</h4>
+                  <TypeIcon type={pin.pinType}/>
+               </div>
+               <p>{pin.description}</p>
+               <div className={style.cardBottomRow}>
+                  <button 
+                     className={style.cardIconWrapper + (pin.upvoted ? " " + style.iconSelected : "")}
+                     onClick={() => upvoteLocation(index)}
+                  >
+                     <ThumbsUp className={style.cardIcon}/>
+                  </button>
+                  <span>{pin.upvotes}</span>
+                  <button 
+                     className={style.cardIconWrapper + (pin.downvoted ? " " + style.iconSelected : "")}
+                     onClick={() => downvoteLocation(index)}
+                  >
+                     <ThumbsDown className={style.cardIcon} />
+                  </button>
+                  <span>{pin.downvotes}</span>
+
+                  <Link
+                     className={style.cardIconWrapper + " " + style.maximizeIcon}
+                     to={`${pin._id}`}
+                  >
+                     <MaximizeIcon className={style.cardIcon}/>
+                  </Link>
+               </div>
             </div>
-            <p style={{ color: pin.color }}>Type: {pin.pinType}</p>
-          </p>
-        ))}
-      </p>
+         ))}
+         </div>
+      </div>
     </div>
   );
 }
